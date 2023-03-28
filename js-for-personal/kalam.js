9c181fdca1a7b5f1678a8a66b47edc8803ae3c34
ABU KALAM <99273096+Abukalam-web@users.noreply.github.com>
2023-03-28 19:54:27 +0600

    dfjdfkjf

---
+++ js-for-personal/kalam.js
@@ -0,0 +1,38 @@
+let deposite = 2940 ;
+let  totalMeal = 66.5 ; 
+function deposite1 () {
+    const kalam00 = document.getElementById("kalam");
+    kalam00.innerHTML = deposite ;
+
+}
+deposite1();
+
+function koroch (){
+    // adding koroch and meal
+    let totalKhoroch = 10581+115+20 ;
+
+    let allTotalMeal = 279 ;
+
+
+
+
+
+
+    const milrate = totalKhoroch / allTotalMeal;
+    const milRate = milrate.toFixed(3);
+    const khoroch1 = milRate * totalMeal ;
+    const khoroch11 = khoroch1.toFixed(2);
+    const getId = document.getElementById("k11");
+    getId.innerHTML = khoroch11; 
+
+    const poket = deposite - khoroch11 ;
+    const poket11 = poket.toFixed(2);
+
+    const poket1 = document.getElementById("poketKalam");
+    poket1.innerHTML = poket11;
+    
+
+
+
+}
+koroch();
--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 
-var kharoch =10581+115 ;
-var motMeal = 000;
+var kharoch =10581+115+20 ;
+var motMeal = 279 + 8.5  ;
 
-// deposit kalam vai 
-var kalamDepo = 2940;
-var kalamMeal = 66.5;
 
-// tnazim khan
-var tanzimD = 1200;
-var tanzimM = 71;
 
-// tahmid 
-var tahmidD  = 2100;
-var tahmidM = 69.5;
 
-// tonmoy 
-var tonmoyD = 2300;
-var tonmoyM = 72;
 
-// kharoch1 = kalamDepo + tanzimD + tahmidD + tonmoyD;
 
-motMeal = kalamMeal + tanzimM + tahmidM + tonmoyM;
+
+// mot meal adding
 var milRate = (kharoch / motMeal);
 var millRate = milRate.toFixed(3);
 
 
 
-
+// for kharoch and meal rate hisab
 function motKharach (){
   const kharach = document.getElementById("motKhoroch");
   kharach.innerHTML = kharoch;
@@ -37,59 +25,6 @@ function mealRate (){
   rate.innerHTML = millRate;
 }
 mealRate();
-
-
-
-
-function kalamVai (){
-  const kalamId = document.getElementById("kalam");
-  kalamId.innerHTML = kalamDepo;
-}
-kalamVai();
-function  k1 (){
-  var k11  = document.getElementById("k11");
-   var k22 = kalamMeal * millRate;
-   k22 = k22.toFixed(2);
-   k11.innerHTML = k22;  
-}
-k1();
-
-// kalam vai end
-
-
-
-// tahmid
-
-
-function allPoket (){
-  var poketKalam = document.getElementById("poketKalam");
-  var poketTanzim = document.getElementById("poketTanzim");
-  var poketTahmid = document.getElementById("poketTahmid");
-  var poketTonmoy = document.getElementById("poketTonmoy");
-
-var poketKalam1 = kalamDepo -(kalamMeal * millRate);
-poketKalam1 =poketKalam1.toFixed(2);
-poketKalam.innerHTML = poketKalam1;
-
-var poketTanzim1 = tanzimD - (tanzimM * millRate);
-poketTanzim1 = poketTanzim1.toFixed(2);
-poketTanzim.innerHTML = poketTanzim1;
-
-var poketTahmid1 = tahmidD - (tahmidM * millRate);
-poketTahmid1 = poketTahmid1.toFixed(2);
-poketTahmid.innerHTML = poketTahmid1;
-
-var poketTonmoy1 = tonmoyD - ( tonmoyM * millRate);
-poketTonmoy1 = poketTonmoy1.toFixed(2);
-poketTonmoy.innerHTML = poketTonmoy1;
- 
-
-
-
-
-}
-allPoket();
-
 
 
 
